4c439c7380ac2cf94c6c0166c886a12ba3114374
Hitesh-180876 <59760324+Hitesh-180876@users.noreply.github.com>
2021-09-10 15:12:27 +0530

    Update problem1.py

--- Lab2- BitManipulation/problem1.py
+++ Lab2- BitManipulation/problem1.py
@@ -1,1 +1,19 @@
+# Hitesh Sharma
+# 180876
+# Assignment 2 
+# Competitive Programming
 
+import sys
+
+T = int(sys.stdin.readline().rstrip())
+for i in range(T):
+    N = int(input())  
+    count = 0  
+    while N>0:
+        if N&1 == 1:
+        count+=1
+        N = N>>1
+        
+    print(count)
+    
+        
