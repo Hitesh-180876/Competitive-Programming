1c98550d9f211270cf62ec71c11346ccc530e229
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2021-09-12 23:04:46 +0530

    Update problem2.py

--- Lab2- BitManipulation/problem2.py
+++ Lab2- BitManipulation/problem2.py
@@ -3,3 +3,19 @@
 # Assignment 1 
 # Competitive Programming
 
+from itertools import combinations
+
+T = int(sys.stdin.readline().rstrip())
+
+for i in range(T):
+    N, K = list(map(int, sys.stdin.readline().rstrip().split()))
+    arr = list(map(int, sys.stdin.readline().rstrip().split()))
+    a = combinations(arr, K)
+    
+    s = 0
+    for i in list(a):
+        s+=sum(list(i))
+    print(s)
+    
+    #print(sum([sum(i) for i in a]))
+
