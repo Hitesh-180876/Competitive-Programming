71461def3b4cf1978480e984ab93a9752621c188
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2021-10-10 17:46:39 +0530

    Update MinMax.py

--- Lab7/MinMax.py
+++ Lab7/MinMax.py
@@ -7,23 +7,14 @@
 def getMinMax(arr):
 	
 	n = len(arr)
-	
-	
 	if(n % 2 == 0):
 		mx = max(arr[0], arr[1])
 		mn = min(arr[0], arr[1])
-		
-		# set the starting index for loop
 		i = 2
-		
-	
 	else:
 		mx = mn = arr[0]
-		
-		
 		i = 1
 		
-
 	while(i < n - 1):
 		if arr[i] < arr[i + 1]:
 			mx = max(mx, arr[i + 1])
@@ -31,10 +22,8 @@ def getMinMax(arr):
 		else:
 			mx = max(mx, arr[i])
 			mn = min(mn, arr[i + 1])
-			
-	
 		i += 2
-	
+		
 	return (mx, mn)
 
 if __name__ =='__main__':
