e9586092d16edb05bf32d9ac9765d83fff95c7d5
Hitesh Kumar Sharma <59760324+Hitesh-180876@users.noreply.github.com>
2021-10-13 23:26:04 +0530

    Update priorityQueue.py

--- Lab8/priorityQueue.py
+++ Lab8/priorityQueue.py
@@ -2,3 +2,16 @@
 # competitive Programming
 # Hitesh 180876
 
+import heapq
+
+t = int(input())
+for i in range(t):
+    n = int(input())
+    arr = [int(item) for item in input().strip().split(" ")]
+    h = []
+    for index, item in enumerate(arr):
+        heapq.heappush(h, (-1 * item, -1 * (item+index+1), index+1)) 
+    ans = [0]*n
+    for i in range(n):
+        ans[i] = heapq.heappop(h)[2]
+    print(" ".join(map(str, ans)))
